0b5f6ff9de939e96897a27b703a3c3842ed2b12c
remon25 <70450564+remon25@users.noreply.github.com>
2024-10-09 22:21:00 +0300

    secont commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import {ViteImageOptimizer} from 'vite-plugin-image-optimizer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    ViteImageOptimizer({
-      mozjpeg: {
-        quality: 75,
-      },
-      pngquant: {
-        quality: [0.65, 0.8],
-        speed: 4,
-      },
-      webp: {
-        quality: 50,
-      },
-      svgo: true, // Enable SVG optimization
-    }),
   ],
   build: {
     target: 'esnext', // Target modern browsers
-    minify: 'terser',  // Use Terser for minification
-    },
+    minify: 'esbuild',  // Use Terser for minification
     cssCodeSplit: true, // Split CSS into separate files
   },
   // Uncomment the server proxy if needed
